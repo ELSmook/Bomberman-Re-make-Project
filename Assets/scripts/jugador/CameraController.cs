a790096b9ad6a432120ce2df0bf2b74077a94698
ELSmook <130613758+ELSmook@users.noreply.github.com>
2025-05-14 19:04:13 -0700

    Base commit

---
+++ Assets/scripts/jugador/CameraController.cs
@@ -0,0 +1,297 @@
+﻿
+using UnityEngine;
+
+/*
+    This file has a commented version with details about how each line works. 
+    The commented version contains code that is easier and simpler to read. This file is minified.
+*/
+
+/// <summary>
+/// Camera movement script for third person games.
+/// This Script should not be applied to the camera! It is attached to an empty object and inside
+/// it (as a child object) should be your game's MainCamera.
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    // Configuracion general
+    [Header("Base Camera Config")]
+    [SerializeField] bool clickToMoveCamera = false;
+    //[SerializeField] float sensitivity = 5f;
+    [SerializeField] Vector2 cameraLimit = new Vector2(-45, 40);
+    [Space]
+    [SerializeField] bool canZoom = true;
+    [SerializeField] float ZoomSens = 100;
+    [SerializeField] float MinFieldOfView = 35;
+    [SerializeField] float MaxFieldOfView = 75;
+    [Header("Camera position and rotation configuration")]
+    public Camera Camara;
+    Transform CameraLocation;
+    //Vector3 rotateCamera = Camera.main.transform.forward;
+    //Quaternion MoveCamera = Camera.main.transform.forward;
+    //Configuracion Extra
+    float mouseX;
+    float mouseY;
+    float offsetDistanceY;
+
+    Transform player;
+    [Header("The distance between the player and the camera (Default is X=0, Y=10 and Z=-12)")]
+    public Vector3 CameraDistance;
+    [Header("The fraction in which the camera rotates around (Default is X=2 and Z=2")]
+    public Vector3 Plus;
+    public Vector3 Take;
+    [Header("The rotation limit")]
+    float RotLimitR= 12f;
+    float RotLimitL= -12f;
+
+    private void Awake() {
+        Camara = Camera.main;
+
+        //Quaternion CameraLocation= Camara.transform.rotation;
+    }
+
+
+    void Start()
+    {
+        //print(CameraLocation.position);
+        // Defino varaibles iniciales
+        player = GameObject.FindWithTag("Point").transform;
+        offsetDistanceY = transform.position.y;
+        //CameraPosition= player.position;
+
+        // Bloqueo y esocndo el mouse
+        if ( ! clickToMoveCamera )
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        CameraDistance= new Vector3(0,10,-12);
+        
+
+
+    }
+
+
+    void Update()
+    {
+        //transform.position = player.position + new Vector3(0, 5, -12);
+
+        // Sigo al jugador
+        MoveCamera(CameraDistance);
+
+        // Set camera zoom when mouse wheel is scrolled
+        float ScrollValue = Input.GetAxis("Mouse ScrollWheel");
+        if ( canZoom && ScrollValue != 0)
+        {
+            // Cambio el campo de vision de la camara
+            print(Input.GetAxis("Mouse ScrollWheel"));
+            Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * ZoomSens;
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, MinFieldOfView, MaxFieldOfView);
+        }
+           
+        // Si la opcion de apretar para mover esta activada y NO se aprieta finaliza la funcion
+        if ( clickToMoveCamera)
+        {
+            // Si aprieto el click
+            if (Input.GetAxisRaw("Fire2") == 1)
+            {
+                // Calculo la rotacion de la camara
+                //mouseX += Input.GetAxis("Mouse X") * sensitivity;
+                //mouseY += Input.GetAxis("Mouse Y") * sensitivity;
+                //mouseY = Mathf.Clamp(mouseY, cameraLimit.x, cameraLimit.y);
+
+                // Roto la camara
+                //transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0);
+            //print("3");
+            }
+        }
+        else
+        {
+            // Calculo la rotacion de la camara
+            //mouseX += Input.GetAxis("Mouse X") * sensitivity;
+            //mouseY += Input.GetAxis("Mouse Y") * sensitivity;
+            //mouseY = Mathf.Clamp(mouseY, cameraLimit.x, cameraLimit.y);
+
+            // Roto la camara
+            //transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0);
+
+            print("2");
+        }
+           
+        // Down para
+        if(Input.GetMouseButton(0)){
+            
+            //float PlusY= 45f;
+            //float PlusXrotation= 5f;
+            //float PlusZrotation= 5f;
+
+            Plus= new Vector3(2f,0f,2f);
+            RotLimitL+= 2;
+
+            //RotateCamera(PlusY);
+            //CameraDistance.x-= Plus.x;
+            //CameraDistance.z+= Plus.z; 
+            //MoveCamera(PlusXrotation,PlusZrotation);
+
+            if(RotLimitL>=0f){
+
+                if(RotLimitL>=24f){
+                    // 24 to 36 
+                    CameraDistance.x-= Plus.x;
+                    CameraDistance.z-= Plus.z;
+                    print("Indice");
+                    print(RotLimitL);
+                }
+                else if(RotLimitL>=12f){
+                    // 12 to 24
+                    CameraDistance.x+= Plus.x;
+                    CameraDistance.z-= Plus.z;
+                    print(RotLimitL);
+                    print("No indice");
+
+                    
+                }
+                else{
+                    // From 0 to 12
+                    CameraDistance.x+= Plus.x;
+                    CameraDistance.z+= Plus.z;
+                    print(RotLimitL);
+                }
+
+            }
+            else{
+                // Position from -12 to 0
+                CameraDistance.x-= Plus.x;
+                CameraDistance.z+= Plus.z;
+                print(RotLimitL);
+            }
+            //print(RightRotation.x);  
+
+            if(RotLimitL==36f){
+                RotLimitL=-12f;
+                CameraDistance= new Vector3(0,10,-12);
+
+                Quaternion CameraRotationReset= Camara.transform.rotation;
+                Quaternion newRotation = Quaternion.Euler(CameraRotationReset.eulerAngles.x,0f, CameraRotationReset.eulerAngles.z);
+
+                // Aplicar la nueva rotación
+                Camara.transform.rotation = newRotation;
+
+            }
+
+            float CameraRotation= 15f;
+
+            RotateCamera(CameraRotation);
+
+            print("rotar izq");
+        }
+        if(Input.GetMouseButton(1)){
+            
+
+            //float TakeXrotation= 5f;
+            //float TakeZrotation= 5f;
+            Take= new Vector3(2f,0f,2f);
+            //Vector3 RightRotation = new Vector3(Take.x - RotLimit,0f,2f);
+            RotLimitR-= 2; 
+
+            //Vector3 BreakPointRight1= new Vector3(2f,0f,2f) + player.transform.position;
+            //Vector3 BreakPointRight2= new Vector3(6f,0f,2f) + CameraDistance;
+            //Vector3 BreakPointRight3= new Vector3(6f,0f,2f) + CameraDistance;
+
+            if(RotLimitR<=0f){
+
+                if(RotLimitR<=-24f){
+                    // -24 to -36 
+                    CameraDistance.x+= Take.x;
+                    CameraDistance.z-= Take.z;
+                    print("Indice");
+                    print(RotLimitR);
+                }
+                else if(RotLimitR<=-12f){
+                    // -12 to -24
+                    CameraDistance.x-= Take.x;
+                    CameraDistance.z-= Take.z;
+                    print(RotLimitR);
+                    print("No indice");
+
+                    
+                }
+                else{
+                    // From 0 to -12
+                    CameraDistance.x-= Take.x;
+                    CameraDistance.z+= Take.z;
+                    print(RotLimitR);
+                }
+
+            }
+            else{
+                // Position from 12 to 0
+                CameraDistance.x+= Take.x;
+                CameraDistance.z+= Take.z;
+
+
+
+                print(RotLimitR);
+            }
+            //print(RightRotation.x);  
+
+            if(RotLimitR==-36f){
+                RotLimitR=12f;
+                CameraDistance= new Vector3(0,10,-12);
+
+                Quaternion CameraRotationReset= Camara.transform.rotation;
+                Quaternion newRotation = Quaternion.Euler(CameraRotationReset.eulerAngles.x,0f, CameraRotationReset.eulerAngles.z);
+
+                // Aplicar la nueva rotación
+                Camara.transform.rotation = newRotation;
+
+            }
+
+            float CameraRotation= -15f;
+
+            RotateCamera(CameraRotation);
+
+            //print(BreakPointRight1);
+            //print("c:");
+            //print(Camara.transform.position);
+            
+            
+
+            //RotateCamera(TakeY);
+            //CameraDistance-= Take; 
+            //MoveCamera(TakeXrotation,TakeZrotation);
+            //Camara.transform.position = Vector3.Slerp(CameraDistance, BreakPointRight, 2f);
+
+            //print("rotar der");
+        }
+
+        void RotateCamera(float YRotation){
+            Quaternion CameraRotation = Camara.transform.rotation;
+
+            Quaternion newRotation = Quaternion.Euler(CameraRotation.eulerAngles.x, CameraRotation.eulerAngles.y + YRotation, CameraRotation.eulerAngles.z);
+
+            // Aplicar la nueva rotación
+            Camara.transform.rotation = newRotation;
+
+
+        }
+
+        void MoveCamera(Vector3 CameraDistance){
+
+            transform.position = player.position +  CameraDistance;
+
+        }
+
+        void MoveX()
+        {
+
+        }
+
+        void MoveZ()
+        {
+
+
+        }
+
+    }
+}
---
+++ Assets/scripts/jugador/ThirdPersonController.cs
@@ -0,0 +1,242 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Main script for third-person movement of the character in the game.
+/// Make sure that the object that will receive this script (the player) 
+/// has the Player tag and the Character Controller component.
+/// </summary>
+///
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class ThirdPersonController : MonoBehaviour
+{
+
+    // Configuracion general
+    [Header("Player Configuration")]
+    [SerializeField] float velocity = 5f;
+    [SerializeField] float sprintAdittion = 3.5f;
+    [SerializeField] float jumpForce = 18f;
+    [SerializeField] float jumpTime = 0.85f;
+    [SerializeField] float gravity = 9.8f;
+    float jumpElapsedTime = 0;
+
+    // Configuracion de agachado
+    [Header("Crounch config")]
+    [SerializeField] float CrouchColliderHeight;
+    [SerializeField] Vector3 CrouchCollierCenter;
+    float StandingColliderHeight;
+    Vector3 StandingCollierCenter;
+
+    // Estados del jugador
+    bool isJumping = false;
+    bool isSprinting = false;
+    bool isCrouching = false;
+
+    // Manejo de inputs
+    float inputHorizontal;
+    float inputVertical;
+    bool inputJump;
+    bool inputCrouch;
+    bool inputSprint;
+
+
+    Animator animator;
+    CharacterController cc;
+    Transform player;
+
+    [Header("Bomb Configuration")]
+    public GameObject Bomb;
+    public Vector3 posicion;
+    
+
+    void Start()
+    {
+
+        // Obtengo los componenetes de mi jugador
+        cc = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Point").transform;
+   
+        // Configuro los parametros del collider 
+        StandingColliderHeight = cc.height;
+        StandingCollierCenter =  cc.center;
+
+    }
+
+    // Update is only being used here to identify keys and trigger animations
+    void Update()
+    {
+
+        // Obtengo los inputs del jugador para el movimiento
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+       
+
+
+        // #### Control de animacion ####
+
+        // Control de agacharce
+        inputCrouch = Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.JoystickButton1);
+        if (inputCrouch)
+        {
+            // Cambio el estado del jugador
+            isCrouching = !isCrouching;
+
+            // Cambio la altura y el centro del controller
+            cc.height = isCrouching? CrouchColliderHeight : StandingColliderHeight;
+            cc.center = isCrouching? CrouchCollierCenter  : StandingCollierCenter;
+            
+            // Ejecuto la animacion de agacharce
+            if (animator != null)
+            {
+                // Ejecuto agachar si es necesario
+                //animator.SetBool("crouch", isCrouching);
+            }
+        }
+
+
+        // Control de correr y caminar
+        inputSprint = Input.GetAxis("Fire3") == 1f;
+        if ( cc.isGrounded && animator != null )
+        {
+            // Caminar
+            float minimumSpeed = 0.9f;
+            //animator.SetBool("run", cc.velocity.magnitude > minimumSpeed );
+
+            // Correr
+            isSprinting = cc.velocity.magnitude > minimumSpeed && inputSprint;
+            //animator.SetBool("sprint", isSprinting );
+
+        }
+
+        // Si acciono la tecla de salto cambio el estado de jugador a saltando
+        inputJump = Input.GetAxis("Jump") == 1f;
+        if (inputJump && cc.isGrounded)
+        {
+            isJumping = true;
+ 
+        }
+
+        // Si esto en el aire activo la animacion de estar en el aire
+        if ( animator != null)
+        {
+            //animator.SetBool("air", cc.isGrounded == false);
+        }
+       
+       // Spawn bomb with Z
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            posicion= player.position;
+            GameObject Clon= Instantiate(Bomb, posicion,Quaternion.identity);
+
+            StartCoroutine(Order(Clon)); // Ahora se llama correctamente la espera antes de destruir
+            Debug.Log("boom");
+        }
+
+        HeadHittingDetect();
+
+    }
+
+
+   // Movieminto en fixed update
+    private void FixedUpdate()
+    {
+        // #### Configuracion para salto #### 
+        float directionY = 0;
+        if ( isJumping )
+        {
+
+            //// Agrego una fuerza de salto que se reduce segun el tiempo trascurrido
+
+            // Agrego una fuerza de salto que depende de " (jumpElapsedTime / jumpTime) "
+            directionY = Mathf.SmoothStep(jumpForce, jumpForce * 0.30f, jumpElapsedTime / jumpTime) * Time.deltaTime;
+
+            // Hago que "(jumpElapsedTime / jumpTime)" resulte en un numero un poco mayor para aplicar la fuerza en el siguiente 
+            // frame hasta que se cumpla el jumpTime Necesario
+            jumpElapsedTime += Time.deltaTime;
+            if (jumpElapsedTime >= jumpTime)
+            {
+                isJumping = false;
+                jumpElapsedTime = 0;
+            }
+        }
+
+        // Modifico la direccion en y segun la gravedad
+        directionY  -= gravity * Time.deltaTime;
+
+
+        // #### Rotacion del jugador ####
+
+        // Determino mi velocidad de movimiento, 
+        float velocityAdittion = isSprinting && !isCrouching ? sprintAdittion : -(velocity * 0.50f);
+
+        // Direccion de movimiento en cada eje
+        float directionX = inputHorizontal * (velocity + velocityAdittion) * Time.deltaTime;
+        float directionZ = inputVertical * (velocity + velocityAdittion) * Time.deltaTime;
+
+        // Obtengo la direccion objetivo segun la posicion de la camara
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        // Elimino la componenete y de cada vector
+        forward.y = 0;
+        right.y = 0;
+
+        // Normalizo para tener una amplitud de 1
+        forward.Normalize();
+        right.Normalize();
+
+        // Cambio su amplitud basandome en los inputs del jugador
+        forward *= directionZ;
+        right *= directionX;
+
+        // Si aprieta alguna tecla del los ejes x o z:
+        if (directionX != 0 || directionZ != 0)
+        {
+            // Matetrucos para rotacion fluida
+            float angle = Mathf.Atan2(forward.x + right.x, forward.z + right.z) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0, angle, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+        }
+
+        
+        // Detemrino direcciones de movimeinto y muevo el CharacterController
+        Vector3 verticalDirection = Vector3.up * directionY;
+        Vector3 horizontalDirection = forward + right;
+        Vector3 movemet = verticalDirection + horizontalDirection;
+        cc.Move(movemet);
+
+    }
+
+
+    // Funcion que hace que el jugador temrine su salto si se choca con el techo
+    void HeadHittingDetect()
+    {
+        // Obtiene parametros para crear un reaycast un poco mas alto que el personaje
+        float headHitDistance = 1.1f;
+        Vector3 ccCenter = transform.TransformPoint(cc.center);
+        float hitCalc = cc.height / 2f * headHitDistance;
+
+        // Uncomment this line to see the Ray drawed in your characters head
+        // Debug.DrawRay(ccCenter, Vector3.up * headHeight, Color.red);
+
+        // Si el rayo choca con un objeto con collider
+        if (Physics.Raycast(ccCenter, Vector3.up, hitCalc))
+        {
+            // Restaura el salto
+            jumpElapsedTime = 0;
+            isJumping = false;
+        }
+    }
+
+    IEnumerator Order(GameObject GameObject)
+    {
+        yield return new WaitForSecondsRealtime(5);
+        Destroy(GameObject);
+    }
+
+
+}
